1892876dfac89eed1588db1fd19d21a3a42563c0
penktastal <arnas.matuolis@gmail.com>
2025-02-28 11:28:59 +0200

    Move categorization to output functions

--- include/isvesties_ir_skaiciavimu_funkcijos.h
+++ include/isvesties_ir_skaiciavimu_funkcijos.h
@@ -12,5 +12,6 @@ float galutinis_pazymys_vidurkis(Studentas &studentas);
 float galutinis_pazymys_mediana(Studentas &studentas);
 void isvesties_meniu(std::vector<Studentas>& studentai, const std::string &isvesties_failo_pavadinimas);
 void isvestis(std::vector<Studentas>& studentai, std::ostream& isvesties_metodas, int rodyti_pasirinkimas);
+void studentu_kategorizacija(std::vector<Studentas>& studentai, int rodyti_pasirinkimas, int rusiavimo_pasirinkimas);
 
 #endif //ISVESTIES_IR_SKAICIAVIMU_FUNKCIJOS_H
--- src/isvesties_ir_skaiciavimu_funkcijos.cpp
+++ src/isvesties_ir_skaiciavimu_funkcijos.cpp
@@ -166,3 +166,59 @@ void isvestis(std::vector<Studentas>& studentai, std::ostream& isvesties_metodas
         std::cerr<<"Ivyko klaida isvedant duomenis: "<<e.what()<<"\n";
     }
 }
+
+void studentu_kategorizacija(std::vector<Studentas>& studentai, const int rodyti_pasirinkimas, const int rusiuoti_pagal) {
+    //ekvivalentu "kietekams" ir "vargsiukams", tiesiog formaliau pavadinta
+    std::vector<Studentas> islaike_studentai{};
+    std::vector<Studentas> neislaike_studentai{};
+
+    studentu_galutiniu_pazymiu_skaiciavimas(studentai);
+    if(rusiuoti_pagal==1 || rusiuoti_pagal==2) {
+        std::cout<<"Studentu kategorizacija automatiskai vykdoma pagal vidurki, nes jusu pasirinkimas netinkamas kategorizacijai";
+        studentu_rusiavimas(studentai, 3);
+    } else {
+        studentu_rusiavimas(studentai, rusiuoti_pagal);
+    }
+
+    if(rusiuoti_pagal == 3) {
+        auto partition_iteratorius = std::partition_point(
+            studentai.begin(), studentai.end(),
+            [](const Studentas& studentas) {return studentas.galutinis_vidurkis>=5.0f;}
+            );
+            islaike_studentai.assign(studentai.begin(), partition_iteratorius);
+            neislaike_studentai.assign(partition_iteratorius, studentai.end());
+    } else if(rusiuoti_pagal == 4) {
+        auto partition_iteratorius = std::partition_point(
+            studentai.begin(), studentai.end(),
+            [](const Studentas& studentas) {return studentas.galutinis_vidurkis<=5.0f;}
+            );
+        neislaike_studentai.assign(studentai.begin(), partition_iteratorius);
+        islaike_studentai.assign(partition_iteratorius, studentai.end());
+    }else if(rusiuoti_pagal==5){
+        auto partition_iteratorius = std::partition_point(
+            studentai.begin(), studentai.end(),
+            [](const Studentas& studentas) {return studentas.galutinis_mediana>=5.0f;}
+            );
+            islaike_studentai.assign(studentai.begin(), partition_iteratorius);
+            neislaike_studentai.assign(partition_iteratorius, studentai.end());
+    } else if(rusiuoti_pagal==6){
+        auto partition_iteratorius = std::partition_point(
+            studentai.begin(), studentai.end(),
+            [](const Studentas& studentas) {return studentas.galutinis_mediana<=5.0f;}
+            );
+        neislaike_studentai.assign(studentai.begin(), partition_iteratorius);
+        islaike_studentai.assign(partition_iteratorius, studentai.end());
+    }
+
+    studentai.clear();
+    try {
+        std::ofstream islaike_output("../data/output/islaike_studentai.txt");
+        isvestis(islaike_studentai, islaike_output, rodyti_pasirinkimas);
+        islaike_output.close();
+        std::ofstream neislaike_output("../data/output/neislaike_studentai.txt");
+        isvestis(neislaike_studentai, neislaike_output, rodyti_pasirinkimas);
+        neislaike_output.close();
+    } catch(std::exception& e) {
+        std::cerr<<"Ivyko klaida isvedant i failus failus: "<<e.what()<<"\n";
+    }
+}
